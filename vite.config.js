b1e31d283521ea26b493f027835b6beb34780025
123099663 <123099663@qq.com>
2024-06-12 23:13:13 +0800

    fix image path issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import CopyPlugin from 'vite-plugin-files-copy'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -9,13 +10,32 @@ export default ({ mode }) => {
     // console.log(loadEnv(mode, process.cwd()).VITE_BASE_URL)
     return defineConfig({
         base: loadEnv(mode, process.cwd()).VITE_BASE_URL,
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            CopyPlugin({
+                patterns: [
+                    {
+                        from: 'src/assets', // string 相对项目下相对路径
+                        to: 'dist/assets', // string 相对项目下相对路径
+                    },
+                ],
+            }),
+        ],
         resolve: {
             alias: {
                 // 添加一个别名 "@" 指向 "src" 目录
                 '@': path.resolve(__dirname, 'src'),
                 // 你可以根据需要添加更多别名
             },
+        },
+        build: {
+            terserOptions: {
+                compress: {
+                    drop_console: true, // 生产环境移除console
+                },
+            },
+            outDir: 'dist',
+            assetsDir: 'assets',
         },
     })
 }
